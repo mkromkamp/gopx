5d2d1e816993258f96dccd2a4ed2808a7a8aa35b
Martin Kromkamp <m.kromkamp@gmail.com>
2016-12-24 09:23:38 +0100

    moved creating of new gpx and renamed vars

--- gpx.go
+++ gpx.go
@@ -130,3 +130,14 @@ type Bounds struct {
 	MinLon  float64  `xml:"minlon,attr"`
 	MaxLon  float64  `xml:"maxlon,attr"`
 }
+
+// NewGpx creates and returns a new Gpx objects.
+func NewGpx() *Gpx {
+	gpx := new(Gpx)
+	gpx.XMLNs = "http://www.topografix.com/GPX/1/1"
+	gpx.XMLNsXsi = "http://www.w3.org/2001/XMLSchema-instance"
+	gpx.XMLSchemaLoc = "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
+	gpx.Version = "1.1"
+	gpx.Creator = "https://github.com/ptrv/go-gpx"
+	return gpx
+}
--- parse.go
+++ parse.go
@@ -9,33 +9,21 @@ import (
 
 // Parse parses a GPX reader and return a Gpx object.
 func Parse(r io.Reader) (*Gpx, error) {
-	g := NewGpx()
-	d := xml.NewDecoder(r)
-	// d.CharsetReader = charset.NewReaderLabel
-	err := d.Decode(g)
+	gpx := NewGpx()
+	decoder := xml.NewDecoder(r)
+	err := decoder.Decode(gpx)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't parse gpx data: %v", err)
 	}
-	return g, nil
+	return gpx, nil
 }
 
 // ParseFile reads a GPX file and parses it.
 func ParseFile(filepath string) (*Gpx, error) {
-	f, err := os.Open(filepath)
+	file, err := os.Open(filepath)
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
-	return Parse(f)
-}
-
-// NewGpx creates and returns a new Gpx objects.
-func NewGpx() *Gpx {
-	gpx := new(Gpx)
-	gpx.XMLNs = "http://www.topografix.com/GPX/1/1"
-	gpx.XMLNsXsi = "http://www.w3.org/2001/XMLSchema-instance"
-	gpx.XMLSchemaLoc = "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
-	gpx.Version = "1.1"
-	gpx.Creator = "https://github.com/ptrv/go-gpx"
-	return gpx
+	defer file.Close()
+	return Parse(file)
 }
