a4514a5dc576eacf55f52ec5e41b1009ca281f41
Martin Kromkamp <m.kromkamp@gmail.com>
2016-12-27 19:51:57 +0100

    Calculate distance between two points

---
+++ point.go
@@ -0,0 +1,24 @@
+package main
+
+import "math"
+
+// EarthRadiasKm radius of the earth in kilometers
+const EarthRadiasKm = 6371
+
+// Distance calculate the distance between to point in kilometers
+func (source *Point) Distance(dest Point) float64 {
+	dLat := (dest.Lat - source.Lat) * (math.Pi / 180.0)
+	dLon := (dest.Lon - source.Lon) * (math.Pi / 180.0)
+
+	lat1 := source.Lat * (math.Pi / 180.0)
+	lat2 := dest.Lat * (math.Pi / 180.0)
+
+	a1 := math.Sin(dLat/2) * math.Sin(dLat/2)
+	a2 := math.Sin(dLon/2) * math.Sin(dLon/2) * math.Cos(lat1) * math.Cos(lat2)
+
+	a := a1 + a2
+
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	return EarthRadiasKm * c
+}
---
+++ point_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestDistance(t *testing.T) {
+	source := Point{Lat: 37.4219999, Lon: -122.0840575}
+	dest := Point{Lat: 37.7888305, Lon: -122.426465}
+	expectedDistance := 50.730879
+
+	distance := source.Distance(dest)
+
+	// Account for a slight offset, this is ok
+	if !(distance < (expectedDistance+0.1) && distance > (expectedDistance-0.1)) {
+		t.Errorf("Expected a distance of %f but got %f", expectedDistance, distance)
+	}
+}
